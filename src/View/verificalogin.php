2892d4da411592de521dc65c0e00f801c382d6ac
jpbp <jpbp@estudante.ufla.br>
2019-06-11 23:21:08 +0000

    mudada para alterarcao do servidor

--- src/View/verificalogin.php
+++ src/View/verificalogin.php
@@ -1,9 +1,13 @@
 <?php
 // é basicamente o verifica do site
-session_start(); // pega a seção para poder verificar se o usuario não esta autenticado
+
 if(!$_SESSION['usuario']){ // se não existe usuario ele volta pro login
+	session_destroy();
 	header('Location: login.php');
   	exit();
 }
+
+
+
 
 ?>
