2843a44569c423524383adfbafad72ba06c69311
Gabrielle Almeida <gabriell.cuba@computacao.ufla.br>
2019-06-12 10:33:29 -0300

    tela do login finalizada close #2

--- src/View/login.php
+++ src/View/login.php
@@ -1,56 +1,61 @@
-<?php
-session_start(); // pega a seção para poder verificar se o usuario não esta autenticado
-?>
 <!DOCTYPE html>
 <html>
-    
+
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistema de Login - PHP + MySQL - Canal TI</title>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
-    <link rel="stylesheet" href="css/bulma.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/login.css">
+    <title>Login</title>
+    <meta name="author" content="Gabrielle">
+    <link rel="icon" href="imagens/hogwarts.png">
+
+    <!-- Responsivo -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/login.css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/63cd9f4730.js"></script>
 </head>
 
 <body>
-    <section class="hero is-success is-fullheight">
-        <div class="hero-body">
-            <div class="container has-text-centered">
-                <div class="column is-4 is-offset-4">
-                    <h3 class="title has-text-grey">Loja Hogwarts</h3>
-                    <h3 class="title has-text-grey"><a href="https://youtube.com/canaltioficial" target="_blank">Teste</a></h3>
-                    <?php
-                        if(isset($_SESSION['nao_autenticado'])): // verifica se o usuario não esta autenticado e aparece a msg
-                    ?>
-                    <div class="notification is-danger">
-                      <p>ERRO: Usuário ou senha inválidos.</p>
-                    </div>
-                    <?php
-                    endif;
-                    unset($_SESSION['nao_autenticado']); // reseta o não_autentuca para não aparecer a mensagem de erro de ligin
-                    ?>
-                    <div class="box">
-                        <form action="../Controller/controller_login.php" method="POST">
-                            <div class="field">
-                                <div class="control">
-                                    <input name="usuario" type="text" class="input is-large" placeholder="Seu usuário" autofocus="">
-                                </div>
-                            </div>
+    <form action="../Controller/controller_login.php" method="POST">
 
-                            <div class="field">
-                                <div class="control">
-                                    <input name="senha" class="input is-large" type="password" placeholder="Sua senha">
-                                </div>
-                            </div>
-                            <button type="submit" class="button is-block is-link is-large is-fullwidth">Entrar</button>
-                        </form>
+        <div class="login container" id="layoutLogin">
+            <img src="imagens/hogwarts.png" alt="hogwarts">
+
+
+            <!-- Username -->
+            <div class="col-md-11 mb-3">
+                <label for="validationDefaultUsername">Username</label>
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroupPrepend2"><i class="far fa-user"></i></span>
                     </div>
+                    <input name="usuario" type="text" class="form-control" id="validationDefaultUsername" placeholder="Username" aria-describedby="inputGroupPrepend2" required>
+                </div>
+            </div>
+
+            <!-- Password -->
+            <div class="col-md-11 mb-3">
+                <label for="inputPassword6">Password</label>
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroupPrepend2"><i class="fas fa-key"></i></span>
+                    </div>
+                    <input name="senha" type="password" id="inputPassword6" class="form-control" placeholder="Password" aria-describedby="passwordHelpInline" required>
+                </div>
+            </div>
+            <div class="form-row">
+                <div id="botao" class="col-sm-8">
+                    <button type="submit" class="btn btn-success">Entrar</button>
                 </div>
             </div>
         </div>
-    </section>
+    </form>
 </body>
 
 </html>
