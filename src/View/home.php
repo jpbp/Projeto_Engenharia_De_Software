d8876303905ba67a2a9fb2e5b2e21e2c6741150e
Gabrielle Almeida <gabriell.cuba@computacao.ufla.br>
2019-06-12 10:50:29 -0300

    Adicionado para o iframe

---
+++ src/View/home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Home</title>
+    <meta name="author" content="Gabrielle">
+    <link rel="icon" href="imagens/hogwarts.png">
+
+    <!-- Responsivo -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+    <link rel="stylesheet" href="cadastroCliente.css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/63cd9f4730.js"></script>
+</head>
+
+<body>
+    <div class="container">
+
+        <img src="imagens/fotoFundo.png" alt="Hogwarts" height="300px">
+    </div>
+</body>
+
+</html>
