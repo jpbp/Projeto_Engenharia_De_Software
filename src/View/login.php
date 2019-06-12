6d9a7e3cbb126fb856a316297ee54cac81d34e17
jpbp <jpbp@estudante.ufla.br>
2019-06-12 15:23:39 +0000

    verificao do login

--- src/View/login.php
+++ src/View/login.php
@@ -30,32 +30,47 @@
     
             <div class="login container" id="layoutLogin">
                 <img src="imagens/hogwarts.png" alt="hogwarts">
-                
-                
-                <!-- Username -->
-                <div class="col-md-11 mb-3">
-                    <label for="validationDefaultUsername">Username</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="inputGroupPrepend2"><i class="far fa-user"></i></span>
-                        </div>
-                        <input name="usuario" type="text" class="form-control" id="validationDefaultUsername" placeholder="Username" aria-describedby="inputGroupPrepend2" required>
+                <!-- Tratamento de erro -->
+            
+          
+                <div id ="espacamento"> 
+                    <?php
+                        if(isset($_SESSION['nao_autenticado'])): // verifica se o usuario não esta autenticado e aparece a msg
+                    ?>
+                    <div class="alert alert-danger" role="alert">
+                        Usuario ou senha invalido
                     </div>
+                    <?php
+                        endif;
+                        unset($_SESSION['nao_autenticado']); // reseta o não_autentuca para não aparecer a mensagem de erro de ligin
+                    ?>
+
                 </div>
-    
-                <!-- Password -->
-                <div class="col-md-11 mb-3">
-                    <label for="inputPassword6">Password</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="inputGroupPrepend2"><i class="fas fa-key"></i></span>
+                    <!-- Username -->
+                    <div class="col-md-11 mb-3">
+                        <label for="validationDefaultUsername">Username</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="inputGroupPrepend2"><i class="far fa-user"></i></span>
+                            </div>
+                            <input name="usuario" type="text" class="form-control" id="validationDefaultUsername" placeholder="Username" aria-describedby="inputGroupPrepend2" required>
                         </div>
-                        <input name="senha" type="password" id="inputPassword6" class="form-control" placeholder="Password" aria-describedby="passwordHelpInline" required>
                     </div>
-                </div>
-                <div class="form-row">
-                    <div id="botao" class="col-sm-8">
-                        <button type="submit" class="btn btn-success">Entrar</button>
+        
+                    <!-- Password -->
+                    <div class="col-md-11 mb-3">
+                        <label for="inputPassword6">Password</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="inputGroupPrepend2"><i class="fas fa-key"></i></span>
+                            </div>
+                            <input name="senha" type="password" id="inputPassword6" class="form-control" placeholder="Password" aria-describedby="passwordHelpInline" required>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div id="botao" class="col-sm-8">
+                            <button type="submit" class="btn btn-success">Entrar</button>
+                        </div>
                     </div>
                 </div>
             </div>
