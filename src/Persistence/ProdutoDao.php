d9ef0aef47dc9b00e8b60168316099272e86e6de
jpbp <jpbp@estudante.ufla.br>
2019-06-15 20:28:48 +0000

    metodo cadastrar alterado

--- src/Persistence/ProdutoDao.php
+++ src/Persistence/ProdutoDao.php
@@ -4,14 +4,16 @@ class ProdutoDao{
     public function ProdutoDao(){ //construtor da classe
     }
         function cadastrarProduto($Produto,$link){ //cadasrtar Produto no banco de dados
-            $query = "INSERT INTO `produtos` (`idProdutos`, `nome`, `preco`, `Descricao`,`quantidade`) VALUES 
-           (".$Produto->getAtributos().");";
-           
+            $query = "INSERT INTO `produtos` (`idProdutos`, `nome`, `preco`, `Descricao`,`quantidade`) 
+            VALUES  ('%s', '%s', '%d', '%s','%s')";
+
+            $query=sprintf($query,"null",$Produto->getNome(),$Produto->getPreco(),$Produto->getDescricao(),$Produto->getQuantidade());
+           echo $query;
            
            $verifica=true;
    
            if(!mysqli_query($link,$query)){
-               //die ("nao foi possivel salvar".mysqli_error($link));
+               die ("nao foi possivel salvar".mysqli_error($link));
                $verifica=false;
                return $verifica;
            }
