079ee5a175064270c24bdd802cd5e94a5841dcae
jpbp <jpbp@estudante.ufla.br>
2019-06-18 15:17:59 +0000

    pagina de cadastrato pedido criada

---
+++ src/View/CadastrarPedido.php
@@ -0,0 +1,173 @@
+<?php
+include("../Model/Cliente.php");
+include("../Persistence/Conection.php");
+include("../Persistence/ClienteDao.php");
+$c1= new Cliente("","","","","","","","","","","","");
+
+$con = new Conection("localhost","root","","lojahogwarts");
+$con->conectar();
+$cDAO = new ClienteDao();
+$resultado = $cDAO->buscarCliente($c1,$con->getLink());
+
+?>
+
+<html>
+	<head> 
+    <title>Cadastro Pedido</title>
+    <meta name="author" content="Gabrielle">
+    <link rel="icon" href="hogwarts.png">
+
+    <!-- Responsivo -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/cadastroCliente.css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/63cd9f4730.js"></script>
+	</head>
+
+	<body>
+    <body>
+
+<div class="Titulo">
+    <h3>
+        Cadastro de Bruxo
+    </h3>
+</div>
+<main class="principal">
+    <div class="row justify-content-center mb-5">
+        <div class="col-sm-12 col-md-10 col-lg-8">
+            <form method="POST" action="../Controller/controller_CadastrarCliente.php">
+                <div class="form-row">
+                    <div class="form-group col-sm-12">
+
+                        <label for="inputNome">Nome</label>
+                        <input type="text" name="nome" class="form-control" id="inputNome" placeholder="Nome" required>
+                        <div class="valid-feedback">
+                            Looks good!
+                        </div>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-sm-4">
+                        <label for="inputCpf"> CPF </label>
+                        <input type="number" class="form-control" name="cpf" id="inputCpf" placeholder="CPF" required>
+                        <div class="invalid-feedback">
+                            Por Favor insira um CPF válido.
+                        </div>
+                    </div>
+
+                    <div class="form-group col-sm-4">
+                        <label for="inputResidencial"> Residencial </label>
+                        <input type="number" class="form-control" name="residencial" id="inputResidencial" placeholder="Residencial" required>
+                        <div class="invalid-feedback">
+                            Por Favor insira um telefone válido.
+                        </div>
+                    </div>
+                    <div class="form-group col-sm-4">
+                        <label for="inputCelular"> Celular </label>
+                        <input type="number" class="form-control" id="inputCelular" name="celular" placeholder="Celular" required>
+                        <div class="invalid-feedback">
+                            Por Favor insira um celular válido.
+                        </div>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-sm-12">
+                        <label for="inputEmail"> Email </label>
+                        <input type="text" class="form-control" id="inputEmail"  name="email" placeholder="Email" required>
+                        <div class="invalid-feedback">
+                            Por Favor insira um email válido.
+                        </div>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-sm-4">
+                        <label for="inputCep"> CEP </label>
+                        <input type="number" class="form-control" name="cep" id="inputCep" placeholder="CEP" required>
+                        <div class="invalid-feedback">
+                            Por Favor insira um cep válido.
+                        </div>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-sm-8">
+                        <label for="inputEndereco"> Endereço </label>
+                        <input type="text" name="logradouro" class="form-control" id="inputEndereco" placeholder="Endereço" required>
+                        <div class="invalid-feedback">
+                            Por Favor insira um enderecço válido.
+                        </div>
+                    </div>
+                    <div class="form-group col-sm-4">
+                        <label for="inputNumero"> Número </label>
+                        <input type="text" name="numero" class="form-control" id="inputNumero" placeholder="Número" required>
+                        <div class="invalid-feedback">
+                            Por Favor insira um número válido.
+                        </div>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-sm-4">
+                        <label for="inputBairro"> Bairro </label>
+                        <input type="text" class="form-control"  name="bairro" id="inputBairro" placeholder="Bairro" required>
+                        <div class="invalid-feedback">
+                            Por Favor insira um bairro válido.
+                        </div>
+                    </div>
+                    <div class="form-group col-sm-4">
+                        <label for="inputCidade"> Cidade </label>
+                        <input type="text" class="form-control" name="cidade" id="inputCidade" placeholder="Cidade" required>
+                        <div class="invalid-feedback">
+                            Por Favor insira um cidade válido.
+                        </div>
+                    </div>
+                    <div class="form-group col-sm-4">
+                        <label for="inputEstado"> Estado </label>
+                        <select id="inputEstado" class="form-control" name="estado">
+                        <option>Bruxo</option>
+            <?php 
+                while($row_resultado=mysqli_fetch_assoc($resultado)){?>
+                <option value= "<?php echo $row_resultado['cpf'];?>"><?php echo $row_resultado['nome'];?>
+                
+                </option><?php }?>
+                        </select>
+                       
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div id="botao" class="col-sm-8">
+                        <button type="submit" class="btn btn-success">Salvar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</main>
+</body>
+<script>
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+</script>
+
+</html>
