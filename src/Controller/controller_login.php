236010e0849b811fa1895192315fc867d5913846
jpbp <jpbp@estudante.ufla.br>
2019-06-22 18:03:57 +0000

    login arrumado

--- src/Controller/controller_login.php
+++ src/Controller/controller_login.php
@@ -1,40 +1,40 @@
 <?php
 session_start();
 include_once("../Persistence/Conection.php");
+include_once("../Model/Funcionario.php");
+include_once("../Persistence/FuncionarioDao.php");
 
 //conexao com o banco
 $con = new Conection("localhost","root","","lojahogwarts");
 $con->conectar();
-
 
 //pegar dados do formulario
 $usuario = $_POST["usuario"];
 $senha = $_POST["senha"];
 
 
-//verificao do login
-if(empty($usuario) || empty($senha)){
-  $_SESSION['nao_autenticado']=true;
-  header('Location: ../View/login.php'); //se o usuario ou senha não for valido ele volta pra tela de login  
-  exit();
-}
+$f1=new Funcionario("","","","","","","","","","","",$usuario,$senha,"");
 
-$query= "SELECT * FROM Funcionarios WHERE username = '".$usuario."' and senha = '".$senha."' " ; // busca no banco o usuario e senha
+$Fdao= new FuncionarioDao();
+$consulta=$Fdao->verificaLogin($f1,$con->getLink());
+if(mysqli_num_rows($consulta)==1){
+    $row = mysqli_fetch_row($consulta);
+    $_SESSION['usuario']=$row[11];
+    $_SESSION['cpfUser']=$row[1];
+    header('Location: ../View/index.php');
+    exit();
+  }
+  //se não o usuario ou senha não for valido ele volta pra tela de login  
+  else{
+    $_SESSION['nao_autenticado']=true;
+    header('Location: ../View/login.php');
+    exit();
+  }
+  
 
 
-$consulta=mysqli_query($con->getLink(),$query);
 
 //se o usuario e senha forem for validos e achado no bancp ele vai para tela do index
-if(mysqli_num_rows($consulta)==1){
-  $_SESSION['usuario']=$usuario;
-  header('Location: ../View/index.php');
-  exit();
-}
-//se não o usuario ou senha não for valido ele volta pra tela de login  
-else{
-  $_SESSION['nao_autenticado']=true;
-  header('Location: controller_login.php');
-  exit();
-}
+
 
 ?>
