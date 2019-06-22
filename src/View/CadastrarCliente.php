76de95a501cdb866af54509716fb1b08d4e8b12c
Gabrielle Almeida <gabriell.cuba@computacao.ufla.br>
2019-06-22 13:43:52 -0300

    erro no css

--- src/View/CadastrarCliente.php
+++ src/View/CadastrarCliente.php
@@ -185,7 +185,7 @@
         window.addEventListener('load', function() {
             // Fetch all the forms we want to apply custom Bootstrap validation styles to
             var forms = document.getElementsByClassName('needs-validation');
-            // Loop over them and prevent submission
+            // Loop over them and prevents submission
             var validation = Array.prototype.filter.call(forms, function(form) {
                 form.addEventListener('submit', function(event) {
                     if (form.checkValidity() === false) {
