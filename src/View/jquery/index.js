6f918b5cc7572abfb43b020b5b9c725c4543505b
Gabrielle Almeida <gabriell.cuba@computacao.ufla.br>
2019-06-23 18:45:58 -0300

    novo arquivo js

---
+++ src/View/jquery/index.js
@@ -0,0 +1,35 @@
+// cria uma função home onde ao ser chamada pega a tag de id frame e altera o valor do atributo src
+// "document" faz com que o javascript entre no DOM e getElementById procura o elemento com o id descrito dentro dos parentesses
+
+function home() {
+    document.getElementById("frame").src = "home.php";
+}
+
+function CadastrarBruxo() {
+    document.getElementById("frame").src = "CadastrarCliente.php";
+}
+
+function BuscarBruxo() {
+    document.getElementById("frame").src = "BuscarCliente.php";
+}
+
+function CadastrarProduto() {
+
+    document.getElementById("frame").src = "CadastrarProduto.php";
+}
+
+function BuscarProduto() {
+    document.getElementById("frame").src = "BuscarProduto.php";
+}
+
+function CadastrarPedido() {
+    document.getElementById("frame").src = " CadastrarPedido.php";
+}
+
+
+$(document).ready(function() {
+    $('li').click(function() {
+        $('li.active').removeClass("active"); //aqui removemos a class do item anteriormente clicado para que possamos adicionar ao item clicado
+        $(this).addClass("active"); //aqui adicionamos a class ao item clicado
+    });
+});
