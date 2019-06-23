f54d1dc62425ba0ea097bde2e9f9e133f4ea0aa8
Gabrielle Almeida <gabriell.cuba@computacao.ufla.br>
2019-06-23 18:42:10 -0300

    Arrumando o input do Buscar

--- src/View/BuscarCliente.php
+++ src/View/BuscarCliente.php
@@ -4,18 +4,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Buscar Bruxo</title>
-	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	<meta charset="utf-8">
+    <title>Buscar Bruxo</title>
+    <meta name="author" content="Gabrielle">
+    <link rel="icon" href="imagens/hogwarts.png">
+
+    <!-- Responsivo -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap  -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/buscar.css">
+
+	<!-- Icones decorativos-->
+    <script src="https://kit.fontawesome.com/63cd9f4730.js"></script>
 </head>
 <body>
-	<h1>Buscar Bruxo</h1>
-	<div>
-		  <form method="POST" action="../Controller/controller_BuscarCliente.php">
-
-		    <label for="nome"></label>
-		    <input type="text"  name="nome" placeholder="Nome a ser buscado">
-		    <button class="button" type="submit" name="Buscar">Buscar</button><br>
-		</form>	
+	<div class = "Titulo">
+		<h1>Buscar Bruxo</h1>
+	</div>
+	<main>
+		<div>
+			<!-- Código do input buscar-->
+			<form method="POST" action="../Controller/controller_BuscarCliente.php" class = "form-dark">
+				<div class="input-group mb-3 col-sm-6 barraPesquisa">
+					<input type="text" class="form-control " placeholder="Você jura solenemente não fazer nada de bom?" aria-label="Recipient's username" aria-describedby="button-addon2">
+					<div class="input-group-append">
+						<button class="btn btn-outline-secondary" type="submit" id="button-addon2"><i class="fas fa-search"></i></button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</main>
 	<?php
 		if(!empty($_SESSION['busca'])){ //se a busca não estiver vazia 
 
